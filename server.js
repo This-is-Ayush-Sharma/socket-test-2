fd641805949871b16ee9435cc244abe63cacd5e2
Ayush Sharma <ThisIsAyush79@gmail.com>
2022-01-08 13:30:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", (socket) => {
+    // console.log("what is socket ", socket);
+    console.log("socket is active to be connected");
+
+    socket.on("chat", (payload) => {
+        console.log(payload);
+        io.emit("chat", payload);
+    })
+})
+
+server.listen(5000, () => console.log("connected"))
